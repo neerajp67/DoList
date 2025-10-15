b1bd12065276a5a3db0c3e1ab1e6f5a39c0eb862
Neeraj Pandey <neerajpandey025@gmai.com>
2025-10-15 19:57:23 +0530

    created initial file structure

---
+++ src/app/components/scroll-to-top/scroll-to-top.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scroll-to-top',
+  imports: [],
+  templateUrl: './scroll-to-top.component.html',
+  styleUrl: './scroll-to-top.component.scss'
+})
+export class ScrollToTopComponent {
+
+}
